5f1c527d21f940ce7bc74d318199721f4990b0d5
bremea <bremeagames@gmail.com>
2023-07-12 13:13:40 -0400

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1>filebunny</h1>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {},
+		colors: {
+			'outer-space': '#444545',
+			'aquamarine': '#B5FFE9',
+			'mint-green': '#C5E0D8',
+			'silver': '#C9C9C9',
+			'teal-rose': '#CEABB1'
+		}
+	},
+	plugins: []
+};
